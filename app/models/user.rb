0bb4ce302a42a68043b89b26ef60b415edb5fb8e
Sean Lim <seanl30@yahoo.com>
2020-10-10 17:47:18 +1000

    Title: Add Validation For The First Name, Gender & Species.
    Status: Complete.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :affiliations, through: :user_affiliations
   has_many :user_locations, dependent: :destroy
   has_many :locations, through: :user_locations
+
+  validates :first_name, presence:true
+  validates :gender, presence: true
+  validates :species, presence: true
 end
